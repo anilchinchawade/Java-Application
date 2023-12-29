ef4d1ac95a99574fbff24a267d08af3cc0371414
Anil Chinchawade <anilchinchawade1@gmail.com>
2023-12-29 14:41:59 +0530

    HelloWorld Class Added

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    HelloWorld() {
+        System.out.println("The Object of HelloWorld Created");
+    }
+
+    public static void main(String[] args) {
+        HelloWorld o1 = new HelloWorld();
+    }
+}
